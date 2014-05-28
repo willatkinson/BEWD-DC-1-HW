3bf81adbac42303c1c57bcbfaed3fcbb7e2635b0
jasonrhaas <jasonrhaas@gmail.com>
2014-05-27 23:02:33 -0400

    added classwork and homework folders and more files

---
+++ Jason_Haas/classwork/ex_apartment_objects/lib/building.rb
@@ -0,0 +1,23 @@
+#Building Class
+class Building
+	attr_accessor :building_name, :building_address, :apartments
+
+	def initialize (building_name, building_address)
+		@building_name = building_name
+		@building_address = building_address
+		@apartments = []
+	end
+
+	def view_apartments
+		puts "----------------#{@building_name} Apartment list------------------"
+		@apartments.each do |apt|
+			puts "Apartment: #{@apartments.name[apt]}"
+			puts "sqft: #{@apartments.apt_sqft[apt]}	Bedrooms:  #{@apartments.apt_bedrooms[apt]}		Bathrooms:  #{@apartments.apt_bathrooms[apt]}"
+			if @apartments.empty?
+				puts "The apartments is vacant"
+			else
+				puts "#{@apartments.renter[apt]}"
+			end
+		end
+	end
+end
